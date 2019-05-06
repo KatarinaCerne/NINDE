5466b2a6e358e23972ce9de943c33e6d9460cfa6
Katarina Černe <ktrn.crn@gmail.com>
2019-05-06 23:28:37 +0200

    prazna BDF funkcija, še nepreverjena runge kutta metoda reda 4

---
+++ 2.dn/BDF.m
@@ -0,0 +1,8 @@
+function res = BDF(fun, a, b, y0, h)
+%funkcija izraèuna rešitev enaèbe y'=fun(x,y) z implicitno 4-èlensko BDF metodo
+%fun ... funkcija v problemu y'(x)=fun(x,y(x))
+%a, b ... meji intervala, na katerem se nahajajo delilne toèke x_i
+%y0 ... zaèetni približek
+%h ... korak iteracije
+
+end
---
+++ 2.dn/RungeKutta4.m
@@ -0,0 +1,23 @@
+function Y = RungeKutta4(fun, a, b, y0, h)
+%funkcija izraèuna rešitev enaèbe y'=fun(x,y) z runge-kutta metodo reda 4
+%fun ... funkcija v problemu y'(x)=fun(x,y(x))
+%a, b ... meji intervala, na katerem se nahajajo delilne toèke x_i
+%y0 ... zaèetni približek
+%h ... korak iteracije
+
+st_iteracij = (b-a)/h;
+X = linspace(a,b,st_iteracij+1); %vrstica, ki vsebuje x_i
+Y = zeros(1,st_iteracij+1); %vrstica, ki vsebuje y_i
+Y(1)=y0;
+
+for i=2:st_iteracij+1
+   k1 = fun(X(i-1),Y(i-1));
+   k2 = fun(X(i-1)+h/2,Y(i-1)+k1/2);
+   k3 = fun(X(i-1)+h/2,Y(i-1)+k2/2);
+   k4 = fun(X(i-1)+h,Y(i-1)+k3);
+   
+   Y(i)=Y(i-1)+h/6*(k1+2*k2+2*k3+k4);
+end
+
+
+end
