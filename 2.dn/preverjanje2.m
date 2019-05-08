dcf4bfc81fa4a0e797fd0c2d25e440e13c0c8369
Katarina Černe <ktrn.crn@gmail.com>
2019-05-08 22:02:33 +0200

    rešila 2. nalogo, torej BDF metoda. morda je kaj narobe, saj ni preveč natančna

---
+++ 2.dn/preverjanje2.m
@@ -0,0 +1,20 @@
+fun1 = @(t,y) -20.*(y-2);
+y01 = 3;
+a1=0;
+b1=2;
+h1=1/25;
+h2=1/20;
+h3=1/15;
+h4=1/10;
+
+Y=BDF(fun1,a1,b1,y01,h1);
+tocnar = @(x) 2+exp(-20.*x);
+
+st_iteracij1 = (b1-a1)/h1;
+X = linspace(a1,b1,st_iteracij1+1);
+Ytocna = tocnar(X);
+rk4 = RungeKutta4(fun1,a1,b1,y01,h1);
+max(abs(Y-Ytocna));
+
+[t,y] = ode45(fun1,a1:h1:b1,y01);
+max(abs(y-Y'));
