61432f58a6c8b139fe265f2ddf383678e53b72f1
Katarina Černe <ktrn.crn@gmail.com>
2019-05-13 18:37:48 +0200

    popravila minus v začetnem problemu pri 3. nalogi, tako da sedaj ne gre več v neskončnost!

--- 2.dn/primer3.m
+++ 2.dn/primer3.m
@@ -1,5 +1,5 @@
 function Yprime = primer3(x,Y)
 %sistem enaèb 1. reda, preveden iz enaèbe 2. reda, dane v 3. nalogi
 %Y in Yprime sta vrstici
-Yprime=[Y(2),1/3*Y(1)^2*Y(2)+3*Y(1)*x];
+Yprime=[Y(2),1/3*Y(1)^2*Y(2)-3*Y(1)*x];
 end
