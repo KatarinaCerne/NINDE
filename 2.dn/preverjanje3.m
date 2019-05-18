61f14554bd3b3c73dbf9dd0a2af4be9f3a38f439
Katarina Černe <ktrn.crn@gmail.com>
2019-05-19 00:17:57 +0200

    dodala grafe pri 3. nalogi

--- 2.dn/preverjanje3.m
+++ 2.dn/preverjanje3.m
@@ -7,5 +7,14 @@ y30 = [0,1];
 Y31 = MilneSistem(@primer3, a3, b3, y30, h31);
 Y32 = MilneSistem(@primer3, a3, b3, y30, h32);
 
+st_iteracij31 = (b3-a3)/h31;
+X31 = linspace(a3,b3,st_iteracij31+1);
 
+st_iteracij32 = (b3-a3)/h32;
+X32 = linspace(a3,b3,st_iteracij32+1);
+
+plot(X31,Y31(:,1));
+hold on
+plot(X32,Y32(:,1));
+hold off
 
